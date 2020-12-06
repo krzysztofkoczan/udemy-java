20e19d2c138fddd42ea646af1c028763ad9c8a29
krzysztofkoczan <krzysztofkoczan@outlook.com>
2020-12-06 23:17:29 +0100

    Create ParsingValuesFromString.java

---
+++ control-flow-statements/src/ParsingValuesFromString.java
@@ -0,0 +1,15 @@
+public class ParsingValuesFromString {
+    public static void main(String[] args) {
+        String numberAsString = "2020.123";
+        System.out.println("numberAsString = " + numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+        System.out.println("number " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number " + number);
+    }
+}
